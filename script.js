33927fcfbd8015b6a12de632fbdb35c3fcc2383b
aliroble123 <aliroble62@gmail.com>
2026-07-14 14:40:32 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openModal(s) {
-    document.getElementById("modal").style.display = "flex";
-    document.getElementById("modalImg").src = s;
-}
-
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-}
